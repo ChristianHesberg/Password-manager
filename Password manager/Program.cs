858a8fa273ca430b55ef0e9c3439bd33ea3e4e85
ChristianHesberg <cchesberg@gmail.com>
2024-10-01 16:40:39 +0200

    basic encryption/decryption flow

--- Password manager/Program.cs
+++ Password manager/Program.cs
@@ -12,16 +12,29 @@ var username = "peter";
 var password = "p@ssw0rd1!";
 
 EncryptionService encryptionService = new EncryptionService();
-var res = encryptionService.GenerateVaultKey(password, Encoding.UTF8.GetBytes(Constants.SaltInBase64));
-Console.WriteLine(Convert.ToBase64String(res));
+var res = encryptionService.GenerateVaultKey(password, Constants.SaltInBase64);
+Console.WriteLine(Convert.ToBase64String(res.key));
+Console.WriteLine(Convert.ToBase64String(res.salt));
 
 var url = "bob.com";
 var passwordForUrl = "pass4bob";
 
-var vaultItem = new VaultItem
-{
-    Url = En
-}
+var serializedPayload = encryptionService.SerializePayload(url, passwordForUrl);
+var encryptedData = encryptionService.Encrypt(serializedPayload, res.key);
+
+Console.WriteLine($"cipherText: {Convert.ToBase64String(encryptedData.ciphertext)}");
+Console.WriteLine($"nonce: {Convert.ToBase64String(encryptedData.nonce)}");
+Console.WriteLine($"tag: {Convert.ToBase64String(encryptedData.tag)}");
+
+var decryptedData =
+    encryptionService.Decrypt(encryptedData.ciphertext, encryptedData.nonce, encryptedData.tag, res.key);
+
+Console.WriteLine($"plainText: {decryptedData}");
+
+var parsedData = encryptionService.ExtractPartsFromData(decryptedData);
+
+Console.WriteLine($"url: {parsedData.url}");
+Console.WriteLine($"password: {parsedData.password}");
 /*using (var db = new VaultContext())
 {
     db.Add(new PasswordHash() { HashedPassword = "password", Salt = "salt"});
--- Password manager/Services/EncryptionService.cs
+++ Password manager/Services/EncryptionService.cs
@@ -5,10 +5,15 @@ namespace Password_manager.Services;
 
 public class EncryptionService
 {
-    public byte[] GenerateVaultKey(string password, byte[] salt)
-    {
-        Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt, 5000, HashAlgorithmName.SHA512);
-        return pbkdf2.GetBytes(16);
+    public (byte[] key, byte[] salt) GenerateVaultKey(string password, string salt, int keySize = 32, int saltSize = 16, int iterations = 10000)  
+    {  
+        // Generate a unique salt  
+        var encodedSalt = Convert.FromBase64String(salt);
+
+        // Derive the key using PBKDF2  
+        using var pbkdf2 = new Rfc2898DeriveBytes(password, encodedSalt, iterations, HashAlgorithmName.SHA512);
+        var key = pbkdf2.GetBytes(keySize);  
+        return (key, encodedSalt);
     }
     
     public (byte[] ciphertext, byte[] nonce, byte[] tag) Encrypt(string plaintext, byte[] key)
@@ -27,7 +32,7 @@ public class EncryptionService
         return (ciphertext, nonce, tag);
     }
     
-    public static string Decrypt(byte[] ciphertext, byte[] nonce, byte[] tag, byte[] key)
+    public string Decrypt(byte[] ciphertext, byte[] nonce, byte[] tag, byte[] key)
     {
         using var aes = new AesGcm(key, 16);
         var plaintextBytes = new byte[ciphertext.Length];
@@ -35,5 +40,22 @@ public class EncryptionService
         aes.Decrypt(nonce, ciphertext, tag, plaintextBytes);
 
         return Encoding.UTF8.GetString(plaintextBytes);
+    }
+
+    public string SerializePayload(string url, string password)
+    {
+        return $"{url}|{password}";
+    }
+    
+    public (string url, string password) ExtractPartsFromData(string data)  
+    {  
+        // Split the string using the delimiter  
+        var parts = data.Split('|');  
+        if (parts.Length != 2)  
+        {  
+            throw new ArgumentException("The data string is not properly formatted.");  
+        } 
+
+        return (parts[0], parts[1]);  
     }
 }
